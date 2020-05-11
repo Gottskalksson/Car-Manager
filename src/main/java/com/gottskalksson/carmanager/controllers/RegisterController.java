564ded8ae32371adbd248e78f54401080fa6e9d5
Gottskalksson <adam.rolirad@gmail.com>
2020-05-11 13:11:37 +0200

    RegisterController created

--- src/main/java/com/gottskalksson/carmanager/controllers/RegisterController.java
+++ src/main/java/com/gottskalksson/carmanager/controllers/RegisterController.java
@@ -1,4 +1,43 @@
 package com.gottskalksson.carmanager.controllers;
 
+import com.gottskalksson.carmanager.entity.User;
+import com.gottskalksson.carmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
 public class RegisterController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public RegisterController(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/register")
+    public String register (Model model) {
+        model.addAttribute("user", new User());
+        return "register-form";
+    }
+
+    @PostMapping("/register")
+    public String validateUser (@ModelAttribute @Valid User user, BindingResult result) {
+
+        if (!result.hasErrors()){
+            userRepository.save(user);
+            return "redirect: /login";
+        } else {
+            return "register-form";
+        }
+    }
+
+
 }
