beaf88be5f5cfbd96919cc0148c9ea6da6f1c581
Gottskalksson <adam.rolirad@gmail.com>
2020-05-12 13:53:31 +0200

    Added bean LocalResolver

--- src/main/java/com/gottskalksson/carmanager/CarmanagerApplication.java
+++ src/main/java/com/gottskalksson/carmanager/CarmanagerApplication.java
@@ -2,12 +2,24 @@ package com.gottskalksson.carmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class CarmanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarmanagerApplication.class, args);
+    }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
     }
 
 }
