ef84183510013d949ed72df76d6cb280fdf4cbe7
Gottskalksson <adam.rolirad@gmail.com>
2020-05-12 13:54:22 +0200

    Email Validator added

--- src/main/java/com/gottskalksson/carmanager/validator/EmailValidator.java
+++ src/main/java/com/gottskalksson/carmanager/validator/EmailValidator.java
@@ -1,4 +1,27 @@
 package com.gottskalksson.carmanager.validator;
 
-public class EmailValidator {
+import com.gottskalksson.carmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            return !repository.existsByEmail(s);
+        } catch (NullPointerException e) {
+            return true;
+        }
+    }
 }
--- src/main/java/com/gottskalksson/carmanager/validator/UniqueEmail.java
+++ src/main/java/com/gottskalksson/carmanager/validator/UniqueEmail.java
@@ -1,4 +1,16 @@
 package com.gottskalksson.carmanager.validator;
 
-public interface UniqueEmail {
-}
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueEmail {
+    String message() default "{com.gottskalksson.carmanager.validator.UniqueEmail.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {}; }
