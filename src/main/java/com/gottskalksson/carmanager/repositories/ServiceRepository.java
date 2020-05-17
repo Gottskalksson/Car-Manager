b1d3123c6fefb803f70c7a97bfff339121fb5c76
Gottskalksson <adam.rolirad@gmail.com>
2020-05-17 14:59:08 +0200

    Add new method to find last services

--- src/main/java/com/gottskalksson/carmanager/repositories/ServiceRepository.java
+++ src/main/java/com/gottskalksson/carmanager/repositories/ServiceRepository.java
@@ -3,5 +3,9 @@ package com.gottskalksson.carmanager.repositories;
 import com.gottskalksson.carmanager.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ServiceRepository extends JpaRepository<Service, Long> {
+
+    List<Service> findFirst5ByOrderByIdDesc();
 }
