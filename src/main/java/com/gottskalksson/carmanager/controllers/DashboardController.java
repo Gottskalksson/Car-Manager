6ac17a4c33e792b5ac1c10aaab3eedc109f235e7
Gottskalksson <adam.rolirad@gmail.com>
2020-05-17 13:51:35 +0200

    File added

--- src/main/java/com/gottskalksson/carmanager/controllers/DashboardController.java
+++ src/main/java/com/gottskalksson/carmanager/controllers/DashboardController.java
@@ -1,4 +1,17 @@
 package com.gottskalksson.carmanager.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping("/dashboard")
+    @ResponseBody
+    public String getDashboard() {
+        return "dashboard";
+    }
+
+
 }
