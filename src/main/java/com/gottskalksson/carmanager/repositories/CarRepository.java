e9e8d9ed5fae5e3ba4954d5bdf05e5af28751960
Gottskalksson <adam.rolirad@gmail.com>
2020-05-20 10:10:10 +0200

    Added new method to find cars by user id

--- src/main/java/com/gottskalksson/carmanager/repositories/CarRepository.java
+++ src/main/java/com/gottskalksson/carmanager/repositories/CarRepository.java
@@ -3,5 +3,9 @@ package com.gottskalksson.carmanager.repositories;
 import com.gottskalksson.carmanager.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+    List<Car> findAllByUserId (long id);
 }
