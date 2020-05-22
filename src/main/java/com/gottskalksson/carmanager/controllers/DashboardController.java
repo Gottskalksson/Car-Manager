d3620f8fe3316aee27c1638b98bdf7054530df03
Gottskalksson <adam.rolirad@gmail.com>
2020-05-22 14:08:36 +0200

    Deleted lines, where get cars list

--- src/main/java/com/gottskalksson/carmanager/controllers/DashboardController.java
+++ src/main/java/com/gottskalksson/carmanager/controllers/DashboardController.java
@@ -36,8 +36,6 @@ public class DashboardController {
         long userId = User.getUserIdFromSession(request);
         List<Service> lastFiveServices = serviceRepository.findFirst5ByUserIdOrderByIdDesc(userId);
         model.addAttribute("serviceList", lastFiveServices);
-        List<Car> carsList = carRepository.findAllByUserId(userId);
-        model.addAttribute("carsList", carsList);
         return "dashboard";
     }
 
